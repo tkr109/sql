7457a2d1fd04d8e7111a731f6e255ed7517a0ce2
tkr109 <103378180+tkr109@users.noreply.github.com>
2023-02-06 22:33:11 +0530

    upload

---
+++ Lab_Assign_3.sql
@@ -0,0 +1,45 @@
+select chr(65) as "character" from dual;
+
+select concat('Hello ','World') as "Concatinated String" from dual;
+
+select instr('Hello world','o') as "Position of o " from dual;
+
+select length('Hiii') as "Length" from dual;
+
+select lpad('Hello',10,'*') as "Left Padding" from dual;
+
+select rpad('Hello',10,'*') as "right Padding" from dual;
+
+select ltrim('      lksdjfa') as "left trim" from dual;
+
+select rtrim('dlsaka        ') as "right trim" from dual;
+
+replace 
+
+substr
+
+select initcap('jdajas') as "Capital Initial Letter" from dual;
+
+select lower('JHHHHJ') from dual;
+
+select upper('slkdjfslkfj') from dual;
+
+select translate('Today is Monday','Monday','Sunday') from dual;
+
+select ceil(2.9) from dual ;
+
+select abs(4-10) from dual;
+
+select cos(10) from dual;
+
+select exp(1) from dual;
+
+select mod(5,4) from dual;
+
+select power(2,2) from dual;
+
+select round(2.5454563,2) from dual;
+
+select sign(10) from dual;
+
+select sqrt(25) from dual;
