ca12314c99a5a518d8d321b9bc19ccf1e3a7e1a8
tkr109 <103378180+tkr109@users.noreply.github.com>
2023-02-08 10:04:58 +0530

    f

--- Lab_Assign_3.sql
+++ Lab_Assign_3.sql
@@ -1,45 +1,60 @@
-select chr(65) as "character" from dual;
+select chr(65) from dual;
 
-select concat('Hello ','World') as "Concatinated String" from dual;
+select concat('hello ','wrld') from dual;
 
-select instr('Hello world','o') as "Position of o " from dual;
+select instr('hello','o') from dual;
 
-select length('Hiii') as "Length" from dual;
+select length('djl') from dual;
 
-select lpad('Hello',10,'*') as "Left Padding" from dual;
+select lpad('hey',10,'*') from dual;
 
-select rpad('Hello',10,'*') as "right Padding" from dual;
+select rpad('hey',10,'*') from dual;
 
-select ltrim('      lksdjfa') as "left trim" from dual;
+select ltrim('      ksdjflskdf') as "dd" from dual;
 
-select rtrim('dlsaka        ') as "right trim" from dual;
+select rtrim('dsfsfs    ') as "sdd" from dual;
 
-replace 
+select replace('000011123','0','l') from dual;  -- llll11123
 
-substr
+select substr('aryan_thakur',3,5) from dual;    --yan_t
 
-select initcap('jdajas') as "Capital Initial Letter" from dual;
+select initcap ('sdljkfsJKJLHKHKJHk') from dual;
 
-select lower('JHHHHJ') from dual;
+select lower('FJFJFJ') FROM DUAL;
 
-select upper('slkdjfslkfj') from dual;
+select upper('kds') from dual;
 
-select translate('Today is Monday','Monday','Sunday') from dual;
+select translate('today is moday','monday','sunday') from dual;
 
-select ceil(2.9) from dual ;
+select sysdate from dual;
 
-select abs(4-10) from dual;
+select last_day(to_date('2003-03-01','yyyy-mm-dd')) from dual;
 
-select cos(10) from dual;
+select months_between(to_date('2003-03-01','yyyy-mm-dd'),to_date('2003-05-01','yyyy-mm-dd')) from dual;
+
+select next_day(to_date('2023-03-01','yyyy-mm-dd'),'monday') from dual;
+
+select greatest(5 ,3 ,6,1) from dual;
+
+select least(5 ,3 ,6,1) from dual;
+
+select abs(3-10) from dual;
+
+select ceil(5.4) from dual;
+
+select floor(5.4) from dual;
 
 select exp(1) from dual;
 
-select mod(5,4) from dual;
+select round('3.34434343433',4) from dual;
 
-select power(2,2) from dual;
+select power(2,3) from dual;
 
-select round(2.5454563,2) from dual;
+select sign('-1') from dual;
 
-select sign(10) from dual;
+select trunc('2.34434343',4) from dual;
 
-select sqrt(25) from dual;
+select mod(6,5) from dual;
+
+select cos(10) from dual;
+
